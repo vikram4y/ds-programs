21868b4f8a48a6d532792a875e3c5a6c60f3bfa0
vikram-lloyds <vikram.kumar@lloydsbanking.com>
2024-10-14 11:44:16 +0530

    ReverseWordsInSentence

--- src/main/java/com/vikram/ds/programs/strings/easy/ReverseWordsInSentence.java
+++ src/main/java/com/vikram/ds/programs/strings/easy/ReverseWordsInSentence.java
@@ -1,2 +1,40 @@
-package com.vikram.ds.programs.strings.easy;public class ReverseWordsInSentence {
+package com.vikram.ds.programs.strings.easy;
+
+import java.util.Stack;
+
+public class ReverseWordsInSentence {
+    public static void main(String[] args) {
+        String s = "This is a sentence";
+        System.out.println("Reversed = "+ reversed(s));
+    }
+
+    //Can use split if allowed
+    private static String reversed(String s) {
+        Stack<String> stack = new Stack<>();
+        int left = 0;
+        int right = 0;
+        while (right<s.length()) {
+            if (s.charAt(right) != ' ')
+                right++;
+            else {
+                if (right == 0) {
+                    left = 1;
+                    right = 1;
+                } else {
+                    stack.push(s.substring(left, right));
+                    right++;
+                    left = right;
+                }
+            }
+            if (right == s.length())
+                stack.push(s.substring(left, right));
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+            if (!stack.isEmpty())
+                sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
